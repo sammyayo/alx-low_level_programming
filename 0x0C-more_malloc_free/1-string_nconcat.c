31312f2adae1d58b05be188ca345746a11f68850
sammyayo <oyesanyaayodele15@gmail.com>
2022-04-11 00:18:29 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,8 +16,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (!s1)
 		s2 = "";
+	if (!s2)
+		s2 = "";
 
 	for (len1 = 0; *(s1 + len1); len1++)
+		;
+	for (len2 = 0; *(s2 + len2); len2++)
 		;
 
 	if (len2 < n)
